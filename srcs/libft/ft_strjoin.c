16833e6b1680bf37cf1f1029836947fa2e238586
Neb7 <95025751+Neb7@users.noreply.github.com>
2025-03-10 12:59:18 +0100

    update libft

--- srcs/libft/ft_strjoin.c
+++ srcs/libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: benpicar <benpicar@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 15:57:19 by benpicar          #+#    #+#             */
-/*   Updated: 2024/12/06 16:42:01 by benpicar         ###   ########.fr       */
+/*   Updated: 2025/02/07 15:14:48 by benpicar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	l1;
 	size_t	l2;
 	char	*d;
-	size_t	i;
 
 	if (!s1 || !s2)
 		return (NULL);
@@ -26,17 +25,26 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	d = (char *)malloc(sizeof(char) * (l1 + l2 + 1));
 	if (!d)
 		return (NULL);
-	i = 0;
-	while (i < l1)
-	{
-		d[i] = s1[i];
-		i++;
-	}
-	while (i < l1 + l2)
-	{
-		d[i] = s2[i - l1];
-		i++;
-	}
-	d[i] = 0;
+	ft_memcpy(d, s1, l1);
+	ft_memcpy(d + l1, s2, l2 + 1);
+	return (d);
+}
+
+char	*ft_strjoin_free(char *s1, char const *s2)
+{
+	size_t	l1;
+	size_t	l2;
+	char	*d;
+
+	if (!s1 || !s2)
+		return (free(s1), NULL);
+	l1 = ft_strlen(s1);
+	l2 = ft_strlen(s2);
+	d = (char *)malloc(sizeof(char) * (l1 + l2 + 1));
+	if (!d)
+		return (free(s1), NULL);
+	ft_memcpy(d, s1, l1);
+	ft_memcpy(d + l1, s2, l2 + 1);
+	free(s1);
 	return (d);
 }
